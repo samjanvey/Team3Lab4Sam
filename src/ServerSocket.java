ba5a068b9fe7429081099dd06b8a8182edf99a57
GRamos <GRamos@192.168.1.15>
2020-02-15 14:38:05 -0500

    Fixing error messaging

--- src/ServerSocket.java
+++ src/ServerSocket.java
@@ -20,27 +20,27 @@ import java.net.Socket;
  */
 public class ServerSocket {
 
-    /**
-     * @param args the command line arguments
-     */
-    public ServerSocket() {
-      System.out.println("Webserver Started");
-        try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(80)) {
-            while (true) {
-                System.out.println("Waiting for client request");
-                Socket remote = serverSocket.accept();
-                System.out.println("Connection made");
-                new Thread(new ClientHandler(remote)).start();
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-    
-    
-    
-    public static void main(String[] args) {
-        new ServerSocket();
-    }
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public ServerSocket() {
+//      System.out.println("Webserver Started");
+//        try (java.net.ServerSocket serverSocket = new java.net.ServerSocket(80)) {
+//            while (true) {
+//                System.out.println("Waiting for client request");
+//                Socket remote = serverSocket.accept();
+//                System.out.println("Connection made");
+//                new Thread(new ClientHandler(remote)).start();
+//            }
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+//    
+//    
+//    
+//    public static void main(String[] args) {
+//        new ServerSocket();
+//    }
     
 }

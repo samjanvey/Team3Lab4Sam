17bedafd0283eaede7299017e03dc5aa0875567d
GRamos <GRamos@192.168.1.15>
2020-02-15 14:27:51 -0500

    Fixing error messaging

---
+++ src/Webserver.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import static java.io.FileDescriptor.out;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author GRamos
+ */
+public class Webserver {
+
+    public Webserver() {
+        
+           System.out.println("Webserver Started");
+           try (ServerSocket serverSocket = new ServerSocket(6000)) {
+               while (true) {
+                   System.out.println("Waiting for client request");
+                   Socket remote = serverSocket.accept();
+                   System.out.println("Connection made");
+                   new Thread(new ClientHandler(remote)).start();
+               }
+           } catch (IOException ex) {
+               ex.printStackTrace();
+           }
+    }
+    
+    public static void main(String args[]) {
+           new Webserver();
+     }
+    
+    public class ClientHandler implements Runnable {
+       private final Socket socket;
+       
+       public ClientHandler(Socket socket) {
+           this.socket = socket;
+        }
+        
+       @Override
+       public void run() {
+           System.out.println("\nClientHandler Started for " +
+               this.socket);
+           handleRequest(this.socket);
+           System.out.println("ClientHandler Terminated for "
+               + this.socket + "\n");
+       }
+    }
+     
+     
+    public void handleRequest(Socket socket) {
+           try (BufferedReader in = new BufferedReader(
+                   new InputStreamReader(socket.getInputStream()));) {
+               String headerLine = in.readLine();
+               StringTokenizer tokenizer = new StringTokenizer(headerLine);
+               String httpMethod = tokenizer.nextToken();
+               
+               if (httpMethod.equals("GET")) {
+                    System.out.println("Get method processed");
+                    String httpQueryString = tokenizer.nextToken();
+                    StringBuilder responseBuffer = new StringBuilder();
+                    responseBuffer
+               .append("<html><h1>WebServer Home Page.... </h1><br>")
+               .append("<b>Welcome to my web server!</b><BR>")
+               .append("</html>");
+                    sendResponse(socket, 200, responseBuffer.toString());
+                } 
+               
+               else if (httpMethod.equals("POST")){
+                   System.out.println("Get method processed");
+                   String httpQueryString = tokenizer.nextToken();
+                   StringBuilder responseBuffer = new StringBuilder();
+                   responseBuffer
+                           .append("<html><h1>WebServer</h1><br>")
+                           .append("<b>Welcome to my web server!</b><BR>")
+                           .append("</html>");
+                   sendResponse(socket, 200, responseBuffer.toString());
+               }
+               
+               else {
+                    System.out.println("The HTTP method is not recognized");
+                    sendResponse(socket, 405, "Method Not Allowed");
+                }
+               
+               String httpQueryString = tokenizer.nextToken();
+       
+           } catch (Exception e) {
+               e.printStackTrace();
+           }
+           
+       }
+       public void sendResponse(Socket socket,
+               int statusCode, String responseString) {
+           
+           String statusLine;
+           String serverHeader = "Server: WebServer\r\n";
+           String contentTypeHeader = "Content-Type: text/html\r\n";
+           
+           try (DataOutputStream out =
+                   new DataOutputStream(socket.getOutputStream());) {
+               if (statusCode == 200) {
+               statusLine = "HTTP/1.0 200 OK" + "\r\n";
+               String contentLengthHeader = "Content-Length: "
+                   + responseString.length() + "\r\n";
+               
+               out.writeBytes(statusLine);
+               out.writeBytes(serverHeader);
+               out.writeBytes(contentTypeHeader);
+               out.writeBytes(contentLengthHeader);
+               out.writeBytes("\r\n");
+               out.writeBytes(responseString);
+           } else if (statusCode == 405) {
+               statusLine = "HTTP/1.0 405 Method Not Allowed" + "\r\n";
+               out.writeBytes(statusLine);
+               out.writeBytes("\r\n");
+           } else {
+               statusLine = "HTTP/1.0 404 Not Found" + "\r\n";
+               out.writeBytes(statusLine);
+               out.writeBytes("\r\n");
+           }
+               out.close();
+           } catch (IOException ex) {
+               // Handle exception
+           }
+           
+       }
+       
+}

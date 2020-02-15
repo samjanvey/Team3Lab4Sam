972294a7ab715ba7cb80cb8297f43b740101a60b
GRamos <GRamos@192.168.1.15>
2020-02-15 14:59:41 -0500

    Fixing error messaging

--- src/Webserver.java
+++ src/Webserver.java
@@ -23,7 +23,7 @@ public class Webserver {
     public Webserver() {
         
            System.out.println("Webserver Started");
-           try (ServerSocket serverSocket = new ServerSocket(6000)) {
+           try (ServerSocket serverSocket = new ServerSocket(8080)) {
                while (true) {
                    System.out.println("Waiting for client request");
                    Socket remote = serverSocket.accept();
